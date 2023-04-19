9b51b79605febed2eb07e71bd6936379bffc66e5
Brianna Ligon <desmondsaelee@hotmail.com>
2023-04-18 20:44:52 -0700

    removed comment from typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -30,7 +30,6 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        // save book parameters need edits
         saveBook(authors: [String], desription: String, title: String, bookId: ID!, image: String , link: String ): User 
         removeBook(bookId: ID!): User
     }
