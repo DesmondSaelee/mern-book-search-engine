7a2411f0af87375ee007d3ba7801bf280b290b5a
Brianna Ligon <desmondsaelee@hotmail.com>
2023-04-19 21:29:14 -0700

    connecting to mongo atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
